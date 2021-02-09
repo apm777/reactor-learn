4fb0dbe069c160e97349454ac8e5eb802a9c81b7
Andrey Mikhaylov <andrey.p.mikhaylov@gazprombank.ru>
2021-02-09 16:42:16 +0500

    Форматирование вывода времени

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -53,6 +53,6 @@ fun main() {
 //    val zoneOffset = OffsetDateTime.now(ZoneId.systemDefault()).offset
 //    val ldt = d.atZone(zoneOffset)
 
-    println(d)
-    println(ds)
+    println("Instant - $d")
+    println("Миллисекунды строка - $ds")
 }
